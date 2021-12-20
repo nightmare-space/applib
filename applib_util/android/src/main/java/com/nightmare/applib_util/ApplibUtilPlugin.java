438b4ded5dfff24f33d9a5b604d55ccbda2fa547
mengyanshou <mengyanshou@gmail.com>
2021-12-20 15:06:41 +0800

    add inline flutter plugin

---
+++ applib_util/android/src/main/java/com/nightmare/applib_util/ApplibUtilPlugin.java
@@ -0,0 +1,40 @@
+package com.nightmare.applib_util;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+/** ApplibUtilPlugin */
+public class ApplibUtilPlugin implements FlutterPlugin, MethodCallHandler {
+    /// The MethodChannel that will the communication between Flutter and native Android
+    ///
+    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+    /// when the Flutter Engine is detached from the Activity
+    private MethodChannel channel;
+    int port;
+
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+        channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "apputils");
+        port = AppChannel.startServer(flutterPluginBinding.getApplicationContext());
+        channel.setMethodCallHandler(this);
+    }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("getPort")) {
+            result.success(port);
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
+}
---
+++ applib_util/example/android/app/src/main/java/com/nightmare/applib_util_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.nightmare.applib_util_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
